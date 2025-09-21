952586b95ccdb9cedb07b3bdc527ab4a342b6e0e
mengYen Chu <michu129fr@gmail.com>
2025-09-22 01:07:38 +0200

    W2E3b,c,d complete

---
+++ Week-2/Exercises/triggering.py
@@ -0,0 +1,49 @@
+from expyriment import design, control, stimuli
+control.set_develop_mode()
+
+exp = design.Experiment(name = "Two Squares")
+
+control.initialize(exp)
+
+#define squares
+square_red = stimuli.Rectangle((50,50), (255,0,0))
+square_green = stimuli.Rectangle((50,50), (0,255,0))
+
+#set position of square, red is left
+square_red.position = (-400, 0)
+square_green.position = (0, 0)
+
+#set global speed
+speed = 5
+
+control.start(subject_id=1)
+
+square_red.present(clear=True, update=False)
+square_green.present(clear=False, update=True)
+
+exp.clock.wait(1000) #presents position for 1 second
+
+def time_steps(pixels_per_tick, distance):
+    """Return how many ticks it takes to cover `distance`
+    given a speed in pixels per tick.
+    """
+    if pixels_per_tick <= 0:
+        raise ValueError("Speed must be > 0")
+    return int(distance / pixels_per_tick)
+
+#square_red covers distance between itself and green (factoring in 'radius) and stops upon 'contact'
+for i in range(time_steps(12, 350)):
+    square_red.move((12,0))
+    square_red.present(clear = True, update=False)
+    square_green.present(clear = False, update = True)
+
+#square_green moves upon 'collision' distance variable in timesteps function can be altered to determine how far it travels after contact. set to 350 according to instrucitons
+for i in range(time_steps(4, 350)):
+    square_green.move((4,0))
+    square_red.present(clear = True, update=False)
+    square_green.present(clear = False, update = True)
+
+#display for 1 second
+exp.clock.wait(1000)
+
+control.end()
